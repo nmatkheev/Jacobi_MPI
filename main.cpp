2a8407d89a27e8ca65aa5313ed5c6f66871f3542
Nikolay Matkheev <nicola.matheev@gmail.com>
2015-12-12 01:58:34 +0300

    Compiles, runs but error in worker --- 'double free or corruption'

--- main.cpp
+++ main.cpp
@@ -16,36 +16,24 @@ using Matrix1D = std::vector<double>;
 using Matrix2D = std::vector<std::vector<double> >;
 
 
-
-void init(int N, Matrix2D& A, Matrix1D& F, Matrix1D& X)
-{
-    A.resize(N);
-    F.resize(N);
-    X.resize(N);
-
-    for (size_t i = 0; i<N; i++) {
-        A[i].resize(N);
-    }
-    cout << "Constructor is ok!" << endl;
-}
-
-
-void load_data(Matrix2D& A, Matrix1D& F, Matrix1D& X, int DIMENSION)
+void load_data(Matrix1D& A, Matrix1D& F, Matrix1D& X, int DIMENSION)
 {
     srand(time(0));
-    cout << "Alive";
+//    cout << "Load data entry...";
     for (int i = 0; i < DIMENSION; i++) {
 //        cout << "Alive entry Load_Data" << i << endl;
         for (int j = 0; j < DIMENSION; j++) {
 //            cout << "I alive" << j << endl;
             if (i == j)
-                A[i][j] = 100 * DIMENSION + rand() % 300 * DIMENSION;
+                A[i+j] = 100 * DIMENSION + rand() % 300 * DIMENSION;
             else
-                A[i][j] = 1 + rand() % 100;
+                A[i+j] = 1 + rand() % 10;
+//            cout << A[i+j] << endl;
         }
         F[i] = 1 + rand() % 10;
         X[i] = 1;
     }
+    cout << "Dataload finished!" << endl;
 }
 
 
@@ -78,26 +66,55 @@ void approx_init(Matrix1D& X)
 /// N - размерность матрицы; A[N][N] - матрица коэффициентов, F[N] - столбец свободных членов,
 /// X[N] - начальное приближение, также ответ записывается в X[N];
 
-void solve_worker(int iternum, Matrix2D& A, Matrix1D& X, Matrix1D& F, int startIndex, int endIndex)
-{
-    int col_len = A.at(0).size();
-    Matrix1D TempX(endIndex-startIndex);
+//void solve_worker(int iternum, Matrix1D& A, Matrix1D& X, Matrix1D& F, int startIndex, int endIndex, int N)
+//{
+//    int col_len = N;
+//    Matrix1D TempX(endIndex-startIndex);
+//    cout << "Entry worker, index: " << endIndex << endl;
+//    for (int run = 0; run < iternum; run++)
+//    {
+//        for (int i = startIndex; i <= endIndex; i++)
+//        {
+//            TempX[i] = F[i];
+//            for (int g = 0; g < col_len; g++)
+//            {
+//                if (i != g)
+//                    TempX[i] -= A[i+g] * X[g];
+//                cout << "worker from i,g: [" << i << "," << g << "]" << endl;
+//            }
+//            TempX[i] /= A[i+i];
+//        }
+//        cout << "Eror?" << endl;
+//        // Here was 'norm' calculation - it's deprecated now.
+//    }
+//}
 
+
+void solve_worker(int iternum, Matrix1D* A, Matrix1D* X, Matrix1D* F, int startIndex, int endIndex, int N, int rank)
+{
+    int col_len = N;
+    Matrix1D TempX(endIndex-startIndex);
+    cout << "Entry worker, index: " << endIndex << endl;
     for (int run = 0; run < iternum; run++)
     {
         for (int i = startIndex; i <= endIndex; i++)
         {
-            TempX[i] = F.at(i);
+            TempX[i] = F->at(i);
             for (int g = 0; g < col_len; g++)
             {
                 if (i != g)
-                    TempX[i] -= A.at(i).at(g) * X.at(g);
+                    TempX[i] -= A->at(i+g) * X->at(g);
+//                cout << "worker from i,g: [" << i << "," << g << "]" << endl;
             }
-            TempX[i] /= A.at(i).at(i);
+            TempX[i] /= A->at(i+i);
         }
+        cout << "Eror in proc" << rank << endl;
         // Here was 'norm' calculation - it's deprecated now.
     }
 }
+
+
+
 
 
 int main(int argc, char* argv[])
@@ -108,46 +125,74 @@ int main(int argc, char* argv[])
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    int dimension = 10000;
-//    double startwtime = 0.0;  double endwtime;
-    int startIndex = 0; int endIndex = 0;
+    int iterations = 1; ////////////////////////
+    int dimension  = 100;     ///////////////////////
+    int startIndex = 0;
+    int endIndex   = 0;
+    int chunk      = dimension / (size-1);
 
-    Matrix2D A;
-    Matrix1D F; Matrix1D X;
+    Matrix1D A(dimension * dimension);  //N*N
+    Matrix1D F(dimension);  //N
+    Matrix1D X(dimension);  //N
 
-    const int iterations = 10; ////////////////////////
-
-
-    if (dimension % (size-1) != 0)
-    {
+    if (dimension % (size-1) != 0) {
         MPI_Finalize();
         cout << "Processes_num should be divisor of dimension!";
         return 0;
     }
 
-//    startwtime = MPI_Wtime();
-
-    if (rank == 0)
-    {
+    if (rank == 0) {
         cout << "Main processor, total procs: " << size << endl;
-
-        init(dimension, A, F, X);
+//        init(dimension, A, F, X);
         load_data(A, F, X, dimension);
 
-        MPI_Bcast(&A.front(), dimension, MPI_INT, 0, MPI_COMM_WORLD);
-        cout << "A -- cast completed" << endl;
-        MPI_Bcast(&F.front(), dimension, MPI_INT, 0, MPI_COMM_WORLD);
-        cout << "F -- cast completed" << endl;
-        MPI_Bcast(&X.front(), dimension, MPI_INT, 0, MPI_COMM_WORLD);
-        cout << "X -- cast completed" << endl;
+        for (int i=1; i < size; i++) {
+            startIndex = (dimension / (size - 1)) * (i - 1);
+            endIndex = ((dimension / (size - 1)) * i) - 1;
+            cout << "Master process sends to process#" << i << " values [start, end]: " << startIndex << " - " << endIndex << endl;
+            MPI_Send(&startIndex, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&endIndex, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+//            cout << "Send ok #" << i <<  endl;
+        }
+    }
+    else if (rank != 0) {
+        MPI_Recv(&startIndex, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&endIndex, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        cout << "Process#" << rank << " received values: [" << startIndex << "," << endIndex << endl;
     }
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    cout << "Starting bcast" << endl;
+    MPI_Bcast(&A[0], A.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    cout << "A -- cast completed" << endl;
+    MPI_Bcast(&F[0], F.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    cout << "F -- cast completed" << endl;
+    MPI_Bcast(&X[0], X.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    cout << "X -- cast completed" << endl;
 
-    if (rank != 0)
-    {
-        solve_worker(iterations, A, X, F, startIndex, endIndex);
+    if (rank != 0) {
+        cout << "worker launched from rank:" << rank << endl;
+                Matrix1D TempX(endIndex-startIndex);
+                for (int run = 0; run < iterations; run++)
+                {
+                    for (int i = startIndex; i <= endIndex; i++)
+                    {
+                        TempX[i] = F[i];
+                        for (int g = 0; g < dimension; g++)
+                        {
+                            if (i != g)
+                                TempX[i] -= A[i+g] * X[g];
+//                            cout << "worker from i,g: [" << i << "," << g << "]" << endl;
+                        }
+                        TempX[i] /= A[i+i];
+                    }
+                    cout << "Eror?" << endl;
+                    // Here was 'norm' calculation - it's deprecated now.
+                }
+
+//        solve_worker(iterations, &A, &X, &F, startIndex, endIndex, dimension, rank);
     }
-
+    //solve_worker(iterations, A, X, F, startIndex, endIndex);
     MPI_Barrier(MPI_COMM_WORLD);
 
     cout << "Completed " << endl;
